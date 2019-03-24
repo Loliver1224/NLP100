87839bec874570e9cad6b815552de1d77800ad66
Loliver1224 <swim.01060808@gmail.com>
2019-03-25 04:52:52 +0900

    initial commit

---
+++ Chapter-1/03.py
@@ -0,0 +1,13 @@
+# 03. 円周率
+# "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+# という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+s = s.replace('.', "")
+s = s.replace(',', "")
+s = s.split()
+list = []
+for word in s:
+    list.append(len(word))
+print(list)
+
+# isalpha()を用いてアルファベットのみをカウントする形でもいいかも
